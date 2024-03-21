eca7d3b236e6211a67f9d9d00c6ceae91fda8bae
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-03-21 10:39:01 -0400

    added more models to models.py

--- server/models.py
+++ server/models.py
@@ -8,17 +8,46 @@ db = SQLAlchemy()
 # Models go here!
 
 class FordMustang(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+    __tablename__ = 'ford_mustangs'
 
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String)
     year = db.Column(db.Integer)
     color = db.Column(db.String)
-    username = db.column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(de.DateTime, onupdate=db.func.now())
+    bids = db.relationship('Bid', secondary='ford_mustang_bid_association', back_populates='ford_mustangs')
+    # username = db.column(db.String)
+    # created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # updated_at = db.Column(de.DateTime, onupdate=db.func.now())
+    def __repr__(self):
+        return f'<Mustang by {self.year} {self.color}>'
 
-    def __repr_(self):
-        return f'<Mustang by {self.username}>'
+class Bid(db.Model):
+    __tablename__ = 'bids'
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    user_id = db.Cloumn(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', back_populates='bids')
+
+    def __repr__(self):
+        return f'<Bid ${self.amount} by {self.user.username}>'
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    bids = db.relationship('Bid', backpopulates='user')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+ford_mustang_bid_association = db.Table('ford_mustang_bid_asscoiation',
+    db.Column('ford_mustang_id', db.Ineger, db.ForeignKey('ford_mustangs.id')),
+    db.Column('bid.id', db.Integer, db.ForeignKey('bids.id'))
+)     
+
 
 
