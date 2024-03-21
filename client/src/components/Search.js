cbbe8def1ed598e59ca9a1512771ffa89391d200
Ta'Keya McFadden <takeyamcfadden@gmail.com>
2024-03-21 03:06:34 -0400

    Updates to Search and NewMustangForm. Will test tomorrow.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,20 +1,35 @@
 import React from "react";
 
-function Search({onChange}) {
+function Search({ mustangs, onChange, value = "" }) {
   const handleInputChange = (event) => {
     const { value } = event.target;
     onChange(value);
-    console.log("Searching...");
-  }
+  };
+
+  const filteredMustangs = mustangs.filter((mustang) =>
+    mustang.model.toLowerCase().includes(value.toLowerCase())
+  );
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Mustangs:</label>
       <input
         type="text"
         id="search"
-        placeholder="Type details to search ex. year, color..."
+        placeholder="Type model name to search..."
+        value={value}
         onChange={handleInputChange}
       />
+
+      {value && (
+        <ul>
+          {filteredMustangs.map((mustang) => (
+            <li key={mustang.model}>
+              {mustang.model} {mustang.year} {mustang.color}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
