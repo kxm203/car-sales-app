acd5d21c70bb11f0a4afad9bb091475e9fc911e1
Lavante Cotton <lavanteisaac@gmail.com>
2024-03-25 17:57:58 -0400

    changed the version of browswer router also added login class ignore and i changed seed.py

--- client/src/components/MustangCard.js
+++ client/src/components/MustangCard.js
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 
-function MustangCard({ mustang }) {
-    const [isInStock, setIsInStock] = useState(true);
+function MustangCard({ mustang, toggleStockStatus }) {
+  const [isInStock, setIsInStock] = useState(true);
 
-    const toggleStockStatus = () => {
-        setIsInStock(!isInStock);
-    };
+  const handleClick = () => {
+    setIsInStock(!isInStock);
+    toggleStockStatus(mustang.id);
+  };
 
-    return (
-        <li className="card" data-testid="mustang-item">
-            <img src={mustang.image} alt={mustang.year} />
-            <h4>{mustang.year}, {mustang.color}</h4>
-            <p>Price: ${mustang.price}</p>
-            {isInStock ? (
-                <button className="primary" onClick={toggleStockStatus}>In Stock</button>
-            ) : (
-                <button onClick={toggleStockStatus}>Out of Stock</button>
-            )}
-        </li>
-    );
+  return (
+    <li className="card" data-testid="mustang-item">
+      <img src={mustang.image} alt={mustang.year} />
+      <h4>{mustang.year}, {mustang.color}</h4>
+      <p>Price: ${mustang.price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
+      ) : (
+        <button onClick={handleClick}>Out of Stock</button>
+      )}
+    </li>
+  );
 }
 
 export default MustangCard;
